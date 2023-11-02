eb92c2b19f7ea5b5619f70f05171667949f31f01
Justin Wei <jwei@lcs.on.ca>
2023-11-02 15:31:58 -0400

    Initial Commit

---
+++ ScoringApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ScoringApp
+//
+//  Created by Justin Zack Wei on 2023-11-02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ScoringApp/ScoringAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoringAppApp.swift
+//  ScoringApp
+//
+//  Created by Justin Zack Wei on 2023-11-02.
+//
+
+import SwiftUI
+
+@main
+struct ScoringAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
